9c708b17c8b05e13c08298ded6d4b6280efaa343
Timothy Corbett-Clark <tcorbettclark@@users.sf.net>
2004-08-15 18:06:26 +0000

    screen shots

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 user=tcorbettclark
-files="index.html news.html about.html download.html default.css"
+files="index.html news.html about.html download.html screenshots.html default.css add_new_bug.png home.png list_bugs.png reviewed_bugs.png search_bugs.png view_edit_bug.png"
 destination=/home/groups/m/mi/midge/htdocs/
 
 for f in $files; do
